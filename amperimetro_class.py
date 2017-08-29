780d5185e42a2768b08e99d2b9c824061b07984b
TriSanMon <31328192+TriSanMon@users.noreply.github.com>
2017-08-29 13:12:10 +0200

    Add files via upload

---
+++ amperimetro_class.py
@@ -0,0 +1,12 @@
+import sublime
+import sublime_plugin
+
+class amperimetro:
+	def __init__(self, identif, periodo, espera):
+		self.identif = identif
+		self.periodo = periodo
+		self.espera = espera
+		self.tabla_amp = []
+
+	def medir(amp)
+		self.tabla_amp.append(amp)
---
+++ voltimetro_class.py
@@ -0,0 +1,12 @@
+import sublime
+import sublime_plugin
+
+class voltimetro:
+	def __init__(self, identif, periodo, espera):
+		self.identif = identif
+		self.periodo = periodo
+		self.espera = espera
+		self.tabla_volt = []
+		
+	def medir(volt)
+		self.tabla_volt.append(volt)
