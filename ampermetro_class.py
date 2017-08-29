8e215fa5fcb4ba1cc215fc07cb4be691cc01d2c8
TriSanMon <31328192+TriSanMon@users.noreply.github.com>
2017-08-29 13:33:56 +0200

    Add files via upload

---
+++ ampermetro_class.py
@@ -0,0 +1,18 @@
+import sublime
+import sublime_plugin
+
+
+class voltimetro:
+	def __init__(self, identif, periodo, espera):
+		self.identif = identif
+		self.periodo = periodo
+		self.espera = espera
+		self.tabla_volt = []
+		self.i = 0
+
+	def _medir(volt)
+		for i in self.periodo: 
+				tabla_volt [i] = volt
+				time.sleep(self.espera)
+
+
